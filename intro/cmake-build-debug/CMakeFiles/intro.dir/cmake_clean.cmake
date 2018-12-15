d6baf829e5261cd61a489920e1182e9104bee937
Artem <temazib@gmail.com>
2018-12-15 20:29:46 +0600

    INTRO B

---
+++ intro/cmake-build-debug/CMakeFiles/intro.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intro.dir/main.cpp.obj"
+  "CMakeFiles/intro.dir/module1.cpp.obj"
+  "CMakeFiles/intro.dir/module2.cpp.obj"
+  "CMakeFiles/intro.dir/module3.cpp.obj"
+  "intro.pdb"
+  "intro.exe"
+  "intro.exe.manifest"
+  "libintro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
