30860dd30ec5e42c413c0b5c313fbf61f8f74e2b
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 23:33:26 +0700

    Update SParser.h

--- lab4/SParser.h
+++ lab4/SParser.h
@@ -1,31 +1,46 @@
 #ifndef UNTITLED1_SPARSER_H
 #define UNTITLED1_SPARSER_H
 
+#include <iostream>
 #include <sstream>
 #include <tuple>
 #include <fstream>
 #include <string>
 #include <vector>
+#include "print_tuple.h"
 
 template <typename... Args>
 class SParser {
-    static std::istream &fin;
-    void ParserR () {
-        std::string str;
-        if (fin.eof)
-            return std::tuple<Args...>();
+    int j;
+    static std::ifstream &fin;
+std::string LineNumber (int i) {
+        fin.seekg(0);
+        std::string str1;
+        for (int j = 0; j < i; j++)
+            getline(fin, str1, '\n');
+        return str1;
+    }
+
+public:
+    SParser(std::ifstream &fint, int i) : j(i), fin(fint) {
 
     }
-public:
-    template <typename T>
+
     class iterator {
         SParser *parser = nullptr;
         std::tuple<Args...> record;
-        size_t position = 0;
-
+        int line_position;
     public:
+        iterator (int i, SParser *P) {
+            line_position = i;
+            parser = P;
+            std::string buff = parser -> LineNumber(line_position);
+            CSVParser A;
+            A.Parser(buff, record);
+        }
+
         bool operator == (iterator &iter2) {
-            return parser == iter2;
+            return (line_position == iter2.line_position);
         }
 
         bool operator != (iterator &iter2) {
@@ -33,30 +48,52 @@ public:
         }
 
         iterator &operator ++ () {
-            if (fin.eof) {
-                position++;
-
-            }
+            if (!fin.eof()) {
+                line_position++;
+                std::string buff = parser -> LineNumber(line_position);
+                int const size = sizeof...(Args);
+                CSVParser<size, Args...> A;
+                A.Parser(buff, record);
+            } else
+                line_position = -1;
 
         }
 
-        T operator * () {
-
+        std::tuple<Args...> operator * () {
+            return record;
         }
 
-        T operator -> () {
-
+        std::tuple<Args...>* operator -> () {
+            return &record;
         }
     };
     iterator begin() {
-
+        return iterator(j, this);
     }
 
     iterator end() {
-
+        return iterator(-1, this);
     }
-
 };
-
+template <int index, class... Args> struct CSVParser {
+    void Parser(std::string &str, std::tuple <Args...> &t) {
+        CSVParser <index - 1, Args...> pElement;
+        pElement.Parser(str, t);
+        std::stringstream ss(str), ss1;
+        std::string str1;
+        getline(ss, str1, ',');
+        str = ss.str();
+        if (!str1.empty()) {
+            ss1 << str1;
+            ss1 >> std::get<index>(t);
+        }
+    }
+};
+template <int index, class... Args>
+struct CSVParser <-1, Args...> {
+    void Parser(std::string str, std::tuple <Args...> &t) {
+        return;
+    }
+};
 
 #endif //UNTITLED1_SPARSER_H
