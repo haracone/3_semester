c41966edff02ba4113bb9bafc4568df15a8e01bd
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 10:27:14 +0700

    Update module1.cpp

--- intro/module1.cpp
+++ intro/module1.cpp
@@ -1,10 +1,36 @@
 #include "module1.h"
+#include <iostream>
+#include "module2.h"
+#include <iomanip>
 
-namespace Module1
-{
-    std::string getMyName()
-    {
-        std::string name = "John";
-        return name;
+
+void Module1::insertW(std::string str) {
+    word[str]++;
+    count++;
+}
+
+void Module1::insertT(std::ifstream &s) {
+    Module2 a;
+    std::string ch;
+    while(a.check(s)) {
+        ch = a.getw(s);
+        if (!ch.empty())
+            insertW(ch);
+    }
+}
+
+void Module1::InsertMM() {
+    for(auto i=word.begin(); i!=word.end(); i++)
+        m2.emplace(i -> second, i-> first);
+}
+
+void Module1::PrintMM(std::ofstream &s) {
+    double a;
+    InsertMM();
+    std::multimap<int, std::string>::reverse_iterator i;
+    for(auto i=m2.rbegin(); i!=m2.rend(); i++) {
+        a = i->first;
+        a = a/count * 100;
+        s << std::setw(20) << std::left << i->second << std::setw(20) << std::left <<  i->first << a << "%" <<  "\n";
     }
 }
