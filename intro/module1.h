6b92fff6fb19dacf1538130a68459e1eda280de7
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 10:26:47 +0700

    Update module1.h

--- intro/module1.h
+++ intro/module1.h
@@ -1,11 +1,20 @@
-#ifndef INTRO_MODULE1_H
-#define INTRO_MODULE1_H
+#ifndef INTRO_B_MODULE1_H
+#define INTRO_B_MODULE1_H
 
+#include <map>
 #include <string>
+#include <fstream>
 
-namespace Module1
-{
-    std::string getMyName();
-}
+class Module1  {
+    std::map <std::string, int> word;
+    int count = 0;
+    std::multimap<int, std::string> m2;
 
-#endif //INTRO_MODULE1_H
+public:
+    void insertW(std::string str);
+    void insertT(std::ifstream &s);
+    void InsertMM();
+    void PrintMM(std::ofstream &s);
+};
+
+#endif //INTRO_B_MODULE1_H
