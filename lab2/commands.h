18b98152225a5abc8d2f59370d5b36f37196c79f
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 21:11:15 +0700

    Update commands.h

--- lab2/commands.h
+++ lab2/commands.h
@@ -4,46 +4,56 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include "Contects.h"
 
 class Commands {
 public:
-    virtual void run();
+    virtual void run(Contects *con, std::string str);
 };
 
 class DEFINE : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class POP : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class PUSH : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class SQRT : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class ADD : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class MINUS : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class MULTIPLY : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class DIVIDE : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 class PRINT : public Commands {
-
+public:
+    void run(Contects *con, std::string str) override;
 };
 
 
