b88d129252cbd4ff9ab694273c6295013dabae1a
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 10:25:54 +0700

    Update main.cpp

--- intro/main.cpp
+++ intro/main.cpp
@@ -1,23 +1,13 @@
-#include "module1.h"
-#include "module2.h"
-#include "module3.h"
 #include <iostream>
+#include <fstream>
+#include "module1.h"
 
 int main(int argc, char** argv) {
-    using namespace std;
-    cout << "Hello world!" << "\n";
+    std::ifstream ifs (*(argv + 1), std::ifstream::in);
+    std::ofstream ofs (*(argv + 2), std::ofstream::out);
+    Module1 a;
+    a.insertT(ifs);
+    a.PrintMM(ofs);
 
-    cout << Module1::getMyName() << "\n";
-    cout << Module2::getMyName() << "\n";
-    {
-        using namespace Module1;
-        cout << getMyName() << "\n"; // (A)
-        cout << Module2::getMyName() << "\n";
-    }
-    using namespace Module2; // (B)
-    cout << getMyName() << "\n"; // COMPILATION ERROR (C)
-
-    using Module2::getMyName;
-    cout << getMyName() << "\n"; // (D)
-    cout <<  Module3::getMyName() << "\n";
+    return 0;
 }
