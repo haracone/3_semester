3602eead8cd35cf0de06798286914e60bcfc8604
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 10:27:55 +0700

    Update module2.cpp

--- intro/module2.cpp
+++ intro/module2.cpp
@@ -1,10 +1,21 @@
 #include "module2.h"
+#include <iostream>
 
-namespace Module2
-{
-    std::string getMyName()
-    {
-        std::string name = "James";
-        return name;
+bool Module2::check(std::ifstream &ifs) {
+    if (!ifs.eof())
+        return true;
+    return false;
+}
+
+std::string Module2::getw(std::ifstream &ifs) {
+    std::string str;
+    char c;
+    while((c = ifs.get()) && (c != EOF)) {
+        if (isdigit(c) || isalpha(c))
+            str.insert(str.end(), c);
+        else if (!str.empty()) {
+            return str;
+        }
     }
+    return str;
 }
