2b3dd27f8798bbede36bf7e584f4f49aa547a538
haracone <33616418+haracone@users.noreply.github.com>
2019-01-21 11:16:29 +0700

    Create Contecst.h

---
+++ lab2/Contecst.h
@@ -0,0 +1,33 @@
+//
+// Created by A on 19.01.2019.
+//
+
+#ifndef UNTITLED_CONTECTS_H
+#define UNTITLED_CONTECTS_H
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+
+class Contects {
+    std::map <std::string, double> map;
+    std::stack <double> stack;
+public:
+    void setmap (std::string str, double a) {
+        map.insert(std::pair<std::string, double>(str, a));
+    };
+
+    void pushs(double number) {
+        stack.push(number);
+    };
+
+    double pops() {
+        double number = stack.top();
+        stack.pop();
+        return number;
+    }
+};
+
+
+#endif //UNTITLED_CONTECTS_H
